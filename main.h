83caa9341eaae356feddced0ac5b151dff8a7e53
goody-1 <atundeg@gmail.com>
2024-01-27 20:21:30 +0100

    initialize header file with standard libraries and custom putchar prototype

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+
+/* Project functions' prototypes */
+int _putchar(char c);
+
+#endif /* MAIN_H */
