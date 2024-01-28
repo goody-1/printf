0d3015fc1b3718f52b5acbf5eee8711c7e12443c
27Mikael <vitalybodgan@gmail.com>
2024-01-28 16:54:20 +0200

    Corrected betty errors

--- _printf.c
+++ _printf.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- *_print - prints the user args
+ *_printf - prints the user args
  * @format: the arg passed into _printf
+ * Return: prints the inputed argument
 */
 int _printf(const char *format, ...)
 {
 	/*variable initialisation*/
 	int letter = 0;
 	va_list args;
+
 	va_start(args, format);
-    
+
 	/*identify the format and */
 	/*match to appropriate funtion*/
 	while (format[letter] != '\0')
@@ -36,5 +38,5 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	va_end(args);
+va_end(args);
 }
