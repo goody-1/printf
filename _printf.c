cc456f03f1c6844e3b688df7522d956fb9632dd3
27Mikael <vitalybodgan@gmail.com>
2024-01-28 15:58:16 +0200

    Update _printf.c: Add switch cases for format specifiers  Implemented functions to handle various format specifiers including %c, %s

--- _printf.c
+++ _printf.c
@@ -6,8 +6,33 @@
 */
 int _printf(const char *format, ...)
 {
+    /*variable initialisation*/
+    int letter = 0;
     va_list args;
     va_start(args, format);
+    
+    /*identify the format and */
+    /*match to appropriate funtion*/
+    while ( letter != '\0')
+    {
+        if ( letter = "%")
+        {
+            letter++;
 
-    int argsList = va_arg(args, const char);
+            switch (args[letter])
+            {
+            case "c":
+                /* code */
+                break;
+            case "d":
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
+
+    char argsList = va_arg(args, format);
+    va_end(args)
 }
