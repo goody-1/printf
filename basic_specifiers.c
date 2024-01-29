f2b755b02d87a562a3e50a288c8dea2de250f1cd
goody-1 <atundeg@gmail.com>
2024-01-29 10:47:20 +0100

    adjust _printf function and dependents to return correct number of characters

--- basic_specifiers.c
+++ basic_specifiers.c
@@ -5,12 +5,14 @@
  * @arg: A list of arguments pointing to
  *       the character to be printed.
  */
-void print_char(va_list arg)
+int print_char(va_list arg)
 {
 	char letter;
 
 	letter = va_arg(arg, int);
 	_putchar(letter);
+
+	return (1);
 }
 
 /**
@@ -18,7 +20,7 @@ void print_char(va_list arg)
  * @arg: A list of arguments pointing to
  *       the string to be printed.
  */
-void print_string(va_list arg)
+int print_string(va_list arg)
 {
 	char *str;
 	int i = 0;
@@ -34,4 +36,21 @@ void print_string(va_list arg)
 		}
 	}
 	/* What happens otherwise? */
+
+	return (i);
+}
+
+/**
+ * print_int - Prints an int.
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
+ */
+int print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+	print_number(num);
+
+	return (get_int_length(num));
 }
