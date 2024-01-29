390a70e36fd0b7f76124a39b3663bf0b697fc02b
goody-1 <atundeg@gmail.com>
2024-01-29 15:07:10 +0100

    add helper function  _strcpy

--- helpers.c
+++ helpers.c
@@ -63,6 +63,29 @@ int get_int_length(int num)
 		num /= 10;
 		length++;
 	}
-
 	return (length);
+}
+/**
+* _strcpy - copies the string pointed to by src,
+* including the terminating null byte (\0),
+* to the buffer pointed to by dest
+* @src: source pointer
+* @dest: destination pointer
+* Return: void
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i = 0;
+
+	while (src[len] != '\0')
+		len++;
+
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[len] = '\0';
+
+	return (dest);
 }
